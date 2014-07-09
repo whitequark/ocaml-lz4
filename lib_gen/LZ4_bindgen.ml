a14184c0fe6681176fd15085790d55d6f067d537
Peter Zotov <whitequark@whitequark.org>
2014-07-09 13:51:23 +0400

    cstubs_internals.h is now automatically included.

--- lib_gen/LZ4_bindgen.ml
+++ lib_gen/LZ4_bindgen.ml
@@ -3,7 +3,6 @@ open Ctypes
 let _ =
   let fmt = Format.formatter_of_out_channel (open_out "lib/LZ4_stubs.c") in
   Format.fprintf fmt "#include <caml/mlvalues.h>@.";
-  Format.fprintf fmt "#include <ctypes/cstubs_internals.h>@.";
   Format.fprintf fmt "#include <lz4.h>@.";
   Cstubs.write_c fmt ~prefix:"caml_" (module LZ4_bindings.C);
 
