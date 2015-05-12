8abe646b1992499260242dd740792eb9c34eae04
Francois BERENGER <francois.berenger@inria.fr>
2015-05-12 14:00:12 +0200

    added decompress_buff

--- lib/LZ4.mli
+++ lib/LZ4.mli
@@ -21,7 +21,8 @@ module type S = sig
       Returns the number of bytes actually written in [buf]
       or raises [Input_too_large]
       if [input] is longer than [0x7E000000] bytes
-      or compress_bound of input is larger than [len]. *)
+      or compress_bound of input is larger than [len].
+      Raises [Invalid_argument "LZ4.decompress"] if [length] or [offset] is negative. *)
   val compress_buff : storage -> storage -> offset:int -> length:int -> int
 
   (** [decompress ~length input] returns LZ4-decompressed [input] or raises [Corrupted]
@@ -29,6 +30,15 @@ module type S = sig
       into [length] bytes or less.
       Raises [Invalid_argument "LZ4.decompress"] if [length] is negative. *)
   val decompress : length:int -> storage -> storage
+
+  (** [decompress_buff input output ~offset ~length]
+      writes LZ4-decompressed [input] into [output] starting at offset [offset]
+      or raises [Corrupted]
+      if [input] does not constitute a valid LZ4-compressed stream which uncompresses
+      into [length] bytes or less.
+      Raises [Invalid_argument "LZ4.decompress"] if [length] or [offset] is negative. *)
+  val decompress_buff : storage -> storage -> offset:int -> length:int -> int
+
 end
 
 module Bytes    : S with type storage = Bytes.t
