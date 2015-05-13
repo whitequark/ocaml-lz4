a4027f006bb8292d66d2ea6904af061be56cd287
whitequark <whitequark@whitequark.org>
2015-05-13 15:05:16 +0300

    Bump ctypes dependency.

--- lib_gen/LZ4_bindgen.ml
+++ lib_gen/LZ4_bindgen.ml
@@ -2,7 +2,6 @@ open Ctypes
 
 let _ =
   let fmt = Format.formatter_of_out_channel (open_out "lib/LZ4_stubs.c") in
-  Format.fprintf fmt "#include <caml/mlvalues.h>@.";
   Format.fprintf fmt "#include <lz4.h>@.";
   Cstubs.write_c fmt ~prefix:"caml_" (module LZ4_bindings.C);
 
