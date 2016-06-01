735de45b9b0581be6dd91de6a711caaed7adc974
Jeremy Yallop <yallop@gmail.com>
2016-06-01 12:21:53 +0100

    Update bindings for ctypes-0.6 compatibility.

--- lib_gen/LZ4_bindings.ml
+++ lib_gen/LZ4_bindings.ml
@@ -1,13 +1,13 @@
 open Ctypes
 
 module C(F: Cstubs.FOREIGN) = struct
-  let compressBound = F.foreign "LZ4_compressBound" (int @-> returning int)
-  let b_compress    = F.foreign "LZ4_compress_limitedOutput"
-                                (ocaml_bytes @-> ocaml_bytes @-> int @-> int @-> returning int)
-  let ba_compress   = F.foreign "LZ4_compress_limitedOutput"
-                                (ptr char @-> ptr char @-> int @-> int @-> returning int)
-  let b_decompress  = F.foreign "LZ4_decompress_safe"
-                                (ocaml_bytes @-> ocaml_bytes @-> int @-> int @-> returning int)
-  let ba_decompress = F.foreign "LZ4_decompress_safe"
-                                (ptr char @-> ptr char @-> int @-> int @-> returning int)
+  let compressBound = F.(foreign "LZ4_compressBound" (int @-> returning int))
+  let b_compress    = F.(foreign "LZ4_compress_limitedOutput"
+                                (ocaml_bytes @-> ocaml_bytes @-> int @-> int @-> returning int))
+  let ba_compress   = F.(foreign "LZ4_compress_limitedOutput"
+                                (ptr char @-> ptr char @-> int @-> int @-> returning int))
+  let b_decompress  = F.(foreign "LZ4_decompress_safe"
+                                (ocaml_bytes @-> ocaml_bytes @-> int @-> int @-> returning int))
+  let ba_decompress = F.(foreign "LZ4_decompress_safe"
+                                (ptr char @-> ptr char @-> int @-> int @-> returning int))
 end
