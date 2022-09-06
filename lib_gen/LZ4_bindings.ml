8cd6164394507a8cc5d8bc161005647d70e5d82d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-09-05 20:01:45 -0400

    fix deprecation warning

--- lib_gen/LZ4_bindings.ml
+++ lib_gen/LZ4_bindings.ml
@@ -2,12 +2,13 @@ open Ctypes
 
 module C(F: Cstubs.FOREIGN) = struct
   let compressBound = F.(foreign "LZ4_compressBound" (int @-> returning int))
-  let b_compress    = F.(foreign "LZ4_compress_limitedOutput"
+  let b_compress    = F.(foreign "LZ4_compress_default"
                                 (ocaml_bytes @-> ocaml_bytes @-> int @-> int @-> returning int))
-  let ba_compress   = F.(foreign "LZ4_compress_limitedOutput"
+  let ba_compress   = F.(foreign "LZ4_compress_default"
                                 (ptr char @-> ptr char @-> int @-> int @-> returning int))
   let b_decompress  = F.(foreign "LZ4_decompress_safe"
                                 (ocaml_bytes @-> ocaml_bytes @-> int @-> int @-> returning int))
   let ba_decompress = F.(foreign "LZ4_decompress_safe"
                                 (ptr char @-> ptr char @-> int @-> int @-> returning int))
+  let version_number = F.(foreign "LZ4_versionNumber" (void @-> returning int))
 end
